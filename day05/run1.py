0c391f36942370e167b4969bdadfc86abf35bc6e
Andy Huibers <andyhuibers@google.com>
2020-12-05 08:52:02 -0800

    use max function

--- day05/run1.py
+++ day05/run1.py
@@ -1,22 +1,16 @@
 
-
 seats = open("seats").read().split()
 #seats = ['FBFBBFFRLR'] # test, MSB to LSB
 #seats = ['BBBBBBBRRR'] # B and R are '1', this value is 2^10-1 = 1023
 
 ids = set()
-n_max = 0
-seat_max = ''
 for seat in seats:
   number = 0
   for i in range(10):
     if seat[i] in ["B","R"]:
       number += (2**(10-1-i))
-  if number > n_max:
-    n_max = number
-    seat_max = seat
   ids.add(number)
-print ("max:", n_max, seat_max)
+print ("max:", max(ids))
 
 ss = sorted(list(ids))
 for i in range(len(ss)-1):
